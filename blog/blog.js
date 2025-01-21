836f005f9cc343c0c9cd450c45c32183821a4b23
dgf1979 <drew@dfin.dev>
2025-01-20 18:43:34 -0800

    convert blog post element build to use HTML native template

--- blog/blog.js
+++ blog/blog.js
@@ -1,5 +1,8 @@
 var converter = new showdown.Converter();
 
+/*
+    blogPosts is a dictionary where the key is the markdown file name and the value is an object with the title and date of the blog post.
+*/
 const blogPosts = {
     "blogtest_1.md": {
         title: "Blog Test 1",
@@ -11,6 +14,9 @@ const blogPosts = {
     },
 }
 
+const postContainerElement = document.getElementById("posts");
+const postTemplateElement = document.getElementById("post-template");
+
 for (const [markdownFileName, properties] of Object.entries(blogPosts)) {
     fetch(markdownFileName)
         .then(response => {
@@ -18,12 +24,21 @@ for (const [markdownFileName, properties] of Object.entries(blogPosts)) {
         })
         .then(markdown => {
             var html = converter.makeHtml(markdown);
-            console.debug(properties, html);
-            const element = document.createElement('div');
-            element.innerHTML = html;
-            document.body.appendChild(element);
+
+            addPostMarkup(properties.title, properties.date, html);
         });
 }
+
+function addPostMarkup(title, date, content) {
+    let postElement = postTemplateElement.content.cloneNode(true);
+    // generate an id from title for jump links
+    postElement.querySelector(".post").id = title.replaceAll(" ", "-").toLowerCase();
+    postElement.querySelector(".post-title").textContent = title;
+    postElement.querySelector(".post-date").textContent = date;
+    postElement.querySelector(".post-content").innerHTML = content;
+    postContainerElement.appendChild(postElement);
+}
+
 
 
 
