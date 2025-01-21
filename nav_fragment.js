7ce04dd432929ca154fcca2f760eb2191f441478
dgf1979 <drew@dfin.dev>
2025-01-21 11:01:58 -0800

    load nav fragment css via the js so I can keep things co-located and organized

--- nav_fragment.js
+++ nav_fragment.js
@@ -1,6 +1,18 @@
 const navFragmentFilePath = '/nav_fragment.html';
+const navFragmentCSSFilePath = '/nav_fragment.css';
+
+//just call a function to load your CSS
+//this path should be relative your HTML location
+function loadCSS(filename) {
+    var file = document.createElement("link");
+    file.setAttribute("rel", "stylesheet");
+    file.setAttribute("type", "text/css");
+    file.setAttribute("href", filename);
+    document.head.appendChild(file);
+}
 
 function LoadSiteNavFragment(parentElementID) {
+    loadCSS(navFragmentCSSFilePath);
     parentElement = document.getElementById(parentElementID);
     fetch(navFragmentFilePath)
         .then(response => {
